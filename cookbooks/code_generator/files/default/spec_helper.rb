fa8e58d51be0971b407554edad7c5f7e25deb36a
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2016-11-11 11:59:19 +0000

    Updated to support Windows tests Set the log level in the `spec_helper` to a variable rather than a constant as it was conflicting with one already set. Added a WINODWS_OPTS for testing Modified the `default_spec` tests as ChefSpec must have a platform for testing, it will become a hard error in a future release if one is not specified
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- cookbooks/code_generator/files/default/spec_helper.rb
+++ cookbooks/code_generator/files/default/spec_helper.rb
@@ -3,10 +3,17 @@ require 'chefspec/berkshelf'
 
 at_exit { ChefSpec::Coverage.report! }
 
-LOG_LEVEL = :fatal
+log_level = :fatal
 UBUNTU_OPTS = {
   platform: 'ubuntu',
   version: '16.04',
-  log_level: LOG_LEVEL,
+  log_level: log_level,
   file_cache_path: '/tmp'
+}.freeze
+
+WINDOWS_OPTS = {
+  platform: 'windows',
+  version: '2012R2',
+  log_level: log_level,
+  file_cache_path: 'c:\chef'
 }.freeze
