97e3ca16242a728ae6ced55d7de973b6e024f625
almogcohen <almog1232@gmail.com>
2018-04-03 12:45:10 -0700

    chef version bigger than 12 enforced for code_generator cookbook

--- cookbooks/code_generator/metadata.rb
+++ cookbooks/code_generator/metadata.rb
@@ -6,6 +6,8 @@ description      'Generates the a cookbook for the cookbook-guide'
 long_description 'Generates the a cookbook for the cookbook-guide'
 version          '1.1.2'
 
+chef_version '>= 12'
+
 %w[aix amazon centos fedora freebsd debian oracle mac_os_x redhat suse opensuse opensuseleap ubuntu windows zlinux].each do |os|
   supports os
 end
