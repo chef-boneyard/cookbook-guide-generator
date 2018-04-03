3fde9bd198d8d34f3dc3cebecaac248f9205bf16
JJ Asghar <jj@chef.io>
2018-04-03 14:11:27 -0500

    fixes for foodcritic
    fixes for foodcritic to pass.
    
    Signed-off-by: JJ Asghar <jj@chef.io>

--- cookbooks/code_generator/metadata.rb
+++ cookbooks/code_generator/metadata.rb
@@ -1,10 +1,16 @@
 name             'code_generator'
 maintainer       'Partner Engineering at Chef Software, Inc.'
 maintainer_email 'partnereng@chef.io'
-license          'Apache 2 License'
+license          'Apache-2.0'
 description      'Generates the a cookbook for the cookbook-guide'
 long_description 'Generates the a cookbook for the cookbook-guide'
-version          '1.1.1'
+version          '1.1.2'
+
+chef_version '<= 12'
+
+%w( aix amazon centos fedora freebsd debian oracle mac_os_x redhat suse opensuse opensuseleap ubuntu windows zlinux ).each do |os|
+  supports os
+end
 
 issues_url 'https://github.com/chef-partners/cookbook-guide-generator/issues'
 source_url 'https://github.com/chef-partners/cookbook-guide-generator'
